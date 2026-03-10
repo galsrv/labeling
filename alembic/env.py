f8bc624564fbda2207b823ea156a9b71c929cf58
Aleksei Sukharev <galsrv@gmail.com>
2026-03-10 22:25:14 +0300

    Микро-рефакторинг

--- alembic/env.py
+++ alembic/env.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.core.base import AppBaseClass
-from src.core.config import settings as s
+from core.base import AppBaseClass
+from core.config import settings as s
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
